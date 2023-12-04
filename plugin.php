2dbb8b36db2893bd7f4ee2980f649f02879f30f3
yakuthemes <info@yakuthemes.com>
2023-12-04 23:52:51 +0100

    renamed defined variables

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@
  * License: GPL2
  */
 
-define( 'THEME_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'THEME_PLUGIN', __DIR__ );
+define( 'WPSIX_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'WPSIX_PLUGIN', __DIR__ );
 
 require_once plugin_dir_path( __FILE__ ) . 'inc/elements.php';
